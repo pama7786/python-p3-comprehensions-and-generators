520451aff7b006e42e78aaa7779f4f63924a948e
abdirahman-abass-diss <abdirahman.diss@student.moringaschool.com>
2023-12-20 19:25:41 +0300

    finished

--- lib/list_comprehension.py
+++ lib/list_comprehension.py
@@ -1,9 +1,15 @@
 #!/usr/bin/env python3
 
-    
 def return_evens(num_list):
     return [num for num in num_list if num % 2 == 0]
 
-
 def make_exclamation(sentence_list):
     return [sentence + '!' for sentence in sentence_list]
+
+# Add any additional code or functions here if needed
+
+# This block ensures that the following code is only executed when the script is run,
+# not when it's imported as a module.
+if __name__ == "__main__":
+    # You can add any testing or demonstration code here
+    pass
